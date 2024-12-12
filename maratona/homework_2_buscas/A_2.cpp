693e944d502d671c6159f53a45a472a96db69285
Bianca Paes de Andrade Soares <bpas@WINDOWS.CIN.UFPE.BR>
2024-12-12 10:47:53 -0300

    segunda tentativa hm2 A

--- maratona/homework_2_buscas/A_2.cpp
+++ maratona/homework_2_buscas/A_2.cpp
@@ -1,20 +1,42 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 #define endl "\n"
 using namespace std;
 
-int main(){
-    int n, x, temp; cin >> n >> x;
+int main() {
+    int n, idx1=-1, idx2=-1;
+    long long x, temp; 
+    cin >> n >> x;
+    map<int, pair<int, int>> num;
     vector<int> lista;
-
+    
     for (int i = 0; i < n; i++){
         cin >> temp;
         lista.push_back(temp);
+        num[temp] = make_pair(i, x-temp);
     }
-
-    map<int, pair<int, int>> numeros(lista.begin(), lista.end());
-
-
-
-
+    
+    for (auto [chave, valor] : num){
+        auto target = find(lista.begin(), lista.end(), valor.second);
+        int indice = distance(lista.begin(), target);
+        if (*target == valor.second && indice != valor.first){
+            idx1 = valor.first;
+            idx2 = num[*target].first;
+            
+            cout << idx1 << " " << idx2 << endl;
+            break;
+        } else {
+            target = find(target, lista.end(), valor.second);
+            if (* target == valor.second){
+                idx1 = valor.first;
+                idx2 = num[*target].first;
+                cout << idx1 << " " << idx2 << endl;
+                break;
+            }
+        }
+    }
+    if (idx1 == -1 && idx2 == -1){
+        cout << "IMPOSSIBLE" << endl;
+    }
+    
     return 0;
 }
