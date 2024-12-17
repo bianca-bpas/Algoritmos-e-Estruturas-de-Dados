6157dab3bc3ff84863fc8ae8e75fa0bb81d4c875
Bianca <bpas@cin.ufpe.br>
2024-12-17 09:26:01 -0300

    tentando a letra E - finalmente eliminei o runtimeError

--- maratona/homework_3_grafos/E-the-lakes.cpp
+++ maratona/homework_3_grafos/E-the-lakes.cpp
@@ -8,7 +8,7 @@ vector<int> dy = {0, 1, -1, 0};
 bool valid(int x, int y, vector<vector<int>>& grid){
     int n = grid.size();
     int m = grid[0].size(); 
-    return x >= 0 && x < n && y >= 0 && y < m && grid[x][y] != 0;
+    return x >= 0 && x < n && y >= 0 && y < m && grid[x][y] > 0;
 }
 
 void rec(int x, int y, vector<vector<int>>& grid, int& volume){
@@ -23,48 +23,40 @@ void rec(int x, int y, vector<vector<int>>& grid, int& volume){
     }
 }
 
-bool findWater(vector<vector<int>>& grid, int& x, int& y, int& lastRow) {
-    int n = grid.size();
-    int m = grid[0].size();
-    for (int i = lastRow; i < n; i++) {
-        for (int j = 0; j < m; j++){
-            if (grid[i][j] != 0){
-                x = i; y = j;
-                lastRow = i;
-                return true;
-            }
-
-        }
-    }
-    return false;
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int t; cin >> t;
     for (int i = 0; i < t; i++){
-        bool found_start = false;
         int n, m; cin >> n >> m;
         vector<vector<int>> grid(n, vector<int>(m));
-        pair<int, int> start;
         for (int j = 0; j < n; j++){
             for (int k = 0; k < m; k++){
                 cin >> grid[j][k];
             }
-
         }
         int x, y;
-        int lastRow=0;
+        int lastRow=0, lastColumn=0;
         int volume=0;
-        while (findWater(grid, x, y, lastRow)){
-            rec(x, y, grid, volume);
+
+        bool found_water = false;
+        
+        for (int i = 0; i < n && !found_water; i++){
+            for (int j = 0; j < m && !found_water; j++){
+                if (grid[i][j] > 0){
+                    x = i; y = j;
+                    found_water = true;
+                    break;
+                }
+            }
         }
+        if (found_water){
+            rec(x, y, grid, volume);
+        } 
+
         cout << volume << endl;
     }
-
-
 
     return 0;
 }
